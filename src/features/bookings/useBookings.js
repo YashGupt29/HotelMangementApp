eb6a14723d5f6186e46b8c5a2ec609928cb8b11c
YashGupt29 <yashnitagartala8@gmail.com>
2024-01-09 11:27:02 +0530

    implemented prefetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   //Filter
   const filterValue = searchParams.get("status") || "all";
@@ -17,6 +19,7 @@ export function useBookings() {
   const sortBy = { field, direction };
   //pagination
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  //query
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -25,5 +28,63 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+  //prefetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
+  if (filterValue === "all") {
+    queryClient.prefetchQuery({
+      queryKey: [
+        "bookings",
+        { field: "status", value: "checked-in" },
+        sortBy,
+        page,
+      ],
+      queryFn: () =>
+        getBookings({
+          filter: { field: "status", value: "checked-in" },
+          sortBy,
+          page,
+        }),
+    });
+    queryClient.prefetchQuery({
+      queryKey: [
+        "bookings",
+        { field: "status", value: "checked-out" },
+        sortBy,
+        page,
+      ],
+      queryFn: () =>
+        getBookings({
+          filter: { field: "status", value: "checked-out" },
+          sortBy,
+          page,
+        }),
+    });
+    queryClient.prefetchQuery({
+      queryKey: [
+        "bookings",
+        { field: "status", value: "unconfirmed" },
+        sortBy,
+        page,
+      ],
+      queryFn: () =>
+        getBookings({
+          filter: { field: "status", value: "unconfirmed" },
+          sortBy,
+          page,
+        }),
+    });
+  }
   return { isLoading, bookings, error, count };
 }

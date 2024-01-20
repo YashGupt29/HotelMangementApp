6aa2f5da8070e3d483e89b2e1131f3bf250f05d3
YashGupt29 <yashnitagartala8@gmail.com>
2024-01-20 12:14:24 +0530

    implementing dashboards

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import Spinner from "../../ui/Spinner";
+import { useRecentBookings } from "./useRecentBooking";
+import useRecentStays from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,28 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+export default function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    isLoading: isLoading2,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+  console.log(confirmedStays);
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today's Activity</div>
+      <div>Charts</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useRecentStays() {
+  const [searchParams] = useSearchParams();
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+  const queryDate = subDays(new Date(), numDays).toISOString();
+  const { isLoading, data: stays } = useQuery({
+    queryKey: ["stays", `last-${numDays}`],
+    queryFn: () => getStaysAfterDate(queryDate),
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+  return { isLoading, stays, confirmedStays, numDays };
+}
